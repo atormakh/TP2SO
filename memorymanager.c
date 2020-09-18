5d3650122e4670127e6b7a2dc292ad8392768040
AgustinJerusalinsky <ajerusalinsky@itba.edu.ar>
2020-09-18 01:00:34 -0300

    primer memory manager bitmap

--- memorymanager.c
+++ memorymanager.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "memorymanager.h"
 
 
@@ -7,83 +8,97 @@
 char memory[MEM_SIZE]={0};
 char bitmap [MEM_SIZE/PAGE_SIZE]={0};
 
-int main(int argc, int * args[]){
-    MemoryManager mem;
-    initialize_mem_man(&mem, memory, bitmap,PAGE_SIZE, MEM_SIZE/PAGE_SIZE);
-    printf("hello world\n");
+void printbitmap(){
+    for(int i = 0; i<MEM_SIZE/PAGE_SIZE; i++){
+        printf("%d ",bitmap[i]);
+    }
+    printf("\n");
 }
 
-void initialize_mem_man(MemoryManager * mm, char * memory, char * bitmap, size_t ps, size_t qty){
+int main(int argc, int * args[]){
+    MemoryManager mm;
+    initialize_mem_man(&mm, memory, bitmap,PAGE_SIZE, MEM_SIZE/PAGE_SIZE);
+    char * buffer = (char *)m_alloc(&mm,40);
+    char * buffer2 = (char *)m_alloc(&mm,30);
+    char * buffer3 = (char *)m_alloc(&mm,6);
+    printbitmap();
+    strcpy(buffer,"hola cami");
+    puts(buffer);
+    m_free(&mm,buffer);
+    printbitmap();
+}
+
+void initialize_mem_man(MemoryManager * mm, char * memory, unsigned char * bitmap, size_t ps, size_t qty){
     mm->base=memory;
     mm->bitmap=bitmap;
     mm->page_size = ps;
     mm->pages_q = qty;
 }
 
-void * alloc(MemoryManager * mm, void * ptr, size_t size ){
+void * m_alloc(MemoryManager * mm, size_t size ){
 
     
     uint blocks_q = (size-1)/mm->page_size + 1;
-    if(ptr==NULL){
-        int i = 0;
-        int cont = 0;
-        while( cont < blocks_q && i < mm->pages_q){
-            if(mm->bitmap[i]==TAKEN) cont = 0;
-            else cont++;
-            i++;
-        }
-        if(cont == blocks_q){
-            //SET BLOCKS EN 1
-            //RETORNAR PUNTERO BLOQUE
-            if(cont > i){
-                //tirar error, nos equivocamos en algo
-            }
-            for(int j=i-cont;j<i;j++){
-                mm->bitmap[j] = 1;
-            }
-            return calc_ptr_from_block(i-cont, mm);
+    
+    int i = 0;
+    int cont = 0;
+
+    //  5 bloques
+    
+    //  ---------------------------------
+    //  malloc(1)
+    
+    //  bitmap: 	1 0 0 0 0
+    //  bloques:	1 0 0 0 0
+    
+    //  ---------------------------------
+    //  malloc(2)
+    
+    //  bitmap:	    1 1 1 0 0
+    //  bloques:	1 2 0 0 0
+    
+ 
+    while( cont < blocks_q && i < mm->pages_q){
+        if(mm->bitmap[i]>0) {
+            cont = 0;
+            i+= mm->bitmap[i];    
         }else{
-            //setear erno no sirve aca creo
-            //podemos imprimir un log
-            return NULL;
-        }
-    }
-    else{
-        int block_index = (int)(ptr-mm->base) / mm->page_size;
-        if((ptr-mm->base) % mm->page_size != 0) blocks_q++;
-
-        if(block_index + blocks_q >= mm->pages_q){
-            //error
-            return NULL;
-        }    
-
-        
-        for(int i = 0; i<blocks_q;i++){
-
-            if(mm->bitmap[i+block_index]==TAKEN){
-                //error
-                //return NULL;
-                return alloc(mm, NULL, size);
-            }   
-        }
-        
-        for(int i = 0; i<blocks_q;i++) mm->bitmap[i+block_index]=TAKEN;
-
-
-        return ptr;
+            cont++;
+            i++;
+        } 
         
         
     }
+    if(cont == blocks_q){
+        if(cont > i){
+            //tirar error, nos equivocamos en algo
+        }
+        mm->bitmap[i-cont] = cont;
+        // for(int j=i-cont;j<i;j++){
+        //     mm->bitmap[j] = TAKEN;
+        // }
+        return calc_ptr_from_idx(i-cont, mm);
+    }else{
+        //setear erno no sirve aca creo
+        //podemos imprimir un log
+        return NULL;
+    }
+    
     
     
     
     
 }
 
-void m_free(MemoryManager * mm, void * ptr, size_t size){
-    
+void m_free(MemoryManager * mm, void * ptr){
+    int idx = calc_idx_from_ptr(ptr, mm);
+    mm->bitmap[idx] = FREE;    
 }
 
-void * calc_ptr_from_block(int block_idx, MemoryManager * mm){
+void * calc_ptr_from_idx(int block_idx, MemoryManager * mm){
     return mm->base+block_idx*mm->page_size;
+}
+
+int calc_idx_from_ptr(void * ptr, MemoryManager * mm){
+    return (int)(ptr-mm->base)/mm->page_size;
 }
--- memorymanager.h
+++ memorymanager.h
@@ -2,20 +2,21 @@
 #define M 1048576
 #define G 1073741824
 
-#define PAGE_SIZE K
+#define PAGE_SIZE 5
 
-#define MEM_SIZE G
+#define MEM_SIZE 100
 #define FREE 0
 #define TAKEN 1
 
 typedef struct MemoryManager{
     void * base;
-    char * bitmap;
+    unsigned char * bitmap;
     size_t page_size;
     size_t pages_q;
 }MemoryManager;
 
-void initialize_mem_man(MemoryManager * mm, char * memory, char * bitmap, size_t ps, size_t qty);
-void * alloc(MemoryManager * mm, void * ptr, size_t size );
-void * calc_ptr_from_block(int block_idx, MemoryManager * mm);
-void m_free(MemoryManager * mm, void * ptr, size_t size);
+void initialize_mem_man(MemoryManager * mm, char * memory, unsigned char * bitmap, size_t ps, size_t qty);
+void * m_alloc(MemoryManager * mm, size_t size );
+void * calc_ptr_from_idx(int block_idx, MemoryManager * mm);
+int calc_idx_from_ptr(void * ptr, MemoryManager * mm);
+void m_free(MemoryManager * mm, void * ptr);
